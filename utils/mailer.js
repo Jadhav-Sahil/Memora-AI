16f87c0d6bb15d7c1fbdbb702a591c49af0b9ca2
sahil <sahiljadhav@9645gmail.com>
2026-07-04 17:38:00 +0530

    Create shared mailer for Brevo

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,5 +1,9 @@
 const nodemailer = require("nodemailer");
-
+console.log("BREVO_EMAIL:", process.env.BREVO_EMAIL);
+console.log(
+    "BREVO_SMTP_KEY:",
+    process.env.BREVO_SMTP_KEY ? "Loaded" : "Missing"
+);
 const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
     port: 587,
