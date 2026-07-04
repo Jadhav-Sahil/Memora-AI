1be9f7981e32090687a239c90f8ce0e388853243
sahil <sahiljadhav@9645gmail.com>
2026-07-04 19:54:50 +0530

    new update for the otp

--- xyz.js
+++
@@ -1,8 +0,0 @@
-const brevo = require("@getbrevo/brevo");
-
-const client = new brevo.BrevoClient({
-    apiKey: process.env.BREVO_API_KEY,
-});
-
-const { transactionalEmails } = brevo.Brevo;
-console.log(transactionalEmails);
